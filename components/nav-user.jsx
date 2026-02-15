d8e75479afe8238cf691396c887d6ba85f0aea0d
Shubham sharma <shubham.shrama2006@gmail.com>
2026-02-15 19:47:02 +0530

    Implement logout
    Added cookie handling and routing for user logout.

--- components/nav-user.jsx
+++ components/nav-user.jsx
@@ -1,5 +1,7 @@
 "use client"
 import { Button } from "@/components/ui/button"
+import { cookies } from 'next/headers';
+import { useRouter } from 'next/navigation'
 import {
   IconCreditCard,
   IconDotsVertical,
@@ -32,9 +34,13 @@ import {
 export function NavUser({
   user,
 }) {
+  const router = useRouter()
   const { isMobile } = useSidebar()
   const handlelogout= ()=>{
     console.log("logout")
+    cookies.delete("token")
+    router.push("/")
+    
   }
   return (
     <SidebarMenu>
