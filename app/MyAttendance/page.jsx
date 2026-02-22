0710be6d976ca40beee94f90533c7a72637b7cbf
shubham-sys01 <shubham.shrama2006@gmail.com>
2026-02-22 16:54:33 +0530

    spinner fix

--- app/MyAttendance/page.jsx
+++ app/MyAttendance/page.jsx
@@ -13,13 +13,17 @@ import {
   CardFooter,
   CardHeader,
 } from "@/components/ui/card";
+import { Spinner } from "@/components/ui/spinner";
 const page = () => {
   const [data, setdata] = useState(null);
+  const [Loading, setLoading] = useState(false);
   const fetchdata = async () => {
+    setLoading(true);
     const res = await fetch("/api/subject");
     const response = await res.json();
     console.log(response);
     setdata(response.data);
+    setLoading(true);
   };
   useEffect(() => {
     fetchdata();
@@ -30,46 +34,59 @@ const page = () => {
       <Card className={"text-center font-bold text-2xl m-1"}>
         <CardTitle>Subjectwise Attendance</CardTitle>
       </Card>
-      <div className="p-2.5 flex flex-col gap-3">
-        {!data ? (
-          <>
-            <div>unable to fetch data</div>
-          </>
-        ) : (
-          data.map((sub) => {
-            return (
-              <Card className="@container/card">
-                <CardHeader>
-                  <CardDescription>{sub.subject}</CardDescription>
-                  <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-                    {Math.ceil(
-                      (sub["attended classes"] / sub.totalclasses) * 100,
-                    ).toString()}
-                    %
-                  </CardTitle>
-                  <CardAction>
-                    <Badge variant="outline" className={"p-2"}>
-                      <div
-                        className={` h-3 w-3 rounded-full ${Math.ceil(
-                          (sub["attended classes"] / sub.totalclasses) * 100,
-                        ) < 75 ? "bg-red-500" : "bg-green-500"}`}
-                      ></div>
-                    </Badge>
-                  </CardAction>
-                </CardHeader>
-                <CardFooter className="flex-col items-start gap-1.5 text-sm">
-                  <div className="line-clamp-1 flex gap-2 font-medium">
-                    Total classes : {sub.totalclasses}
-                  </div>
-                  <div className="">
-                    Totended classes : {sub["attended classes"]}
-                  </div>
-                </CardFooter>
-              </Card>
-            );
-          })
-        )}
-      </div>
+      {Loading ? (
+        <>
+        <div className="flex flex-col justify-center items-center">
+          <Spinner className={"h-8"}/>
+        </div>
+        </>
+      ) : (
+        <div className="p-2.5 flex flex-col gap-3">
+          {!data ? (
+            <>
+              <div>unable to fetch data</div>
+            </>
+          ) : (
+            data.map((sub) => {
+              return (
+                <Card className="@container/card">
+                  <CardHeader>
+                    <CardDescription>{sub.subject}</CardDescription>
+                    <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+                      {Math.ceil(
+                        (sub["attended classes"] / sub.totalclasses) * 100,
+                      ).toString()}
+                      %
+                    </CardTitle>
+                    <CardAction>
+                      <Badge variant="outline" className={"p-2"}>
+                        <div
+                          className={` h-3 w-3 rounded-full ${
+                            Math.ceil(
+                              (sub["attended classes"] / sub.totalclasses) *
+                                100,
+                            ) < 75
+                              ? "bg-red-500"
+                              : "bg-green-500"
+                          }`}
+                        ></div>
+                      </Badge>
+                    </CardAction>
+                  </CardHeader>
+                  <CardFooter className="flex-col items-start gap-1.5 text-sm">
+                    <div className="line-clamp-1 flex gap-2 font-medium">
+                      Total classes : {sub.totalclasses}
+                    </div>
+                    <div className="">
+                      Totended classes : {sub["attended classes"]}
+                    </div>
+                  </CardFooter>
+                </Card>
+              );
+            })
+          )}
+        </div>
+      )}
     </SidebarPro>
   );
 };
